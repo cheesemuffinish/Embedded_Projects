455b2c8188c45f1340843b62b235bc1528036b5c
AudreyLong <cheesemuffinish@gmail.com>
2020-02-15 15:45:54 -0500

    added more code

--- Project2_Serial_Temperature/Serial_Temperature/Serial_Temperature.ino
+++ Project2_Serial_Temperature/Serial_Temperature/Serial_Temperature.ino
@@ -1,10 +1,12 @@
 #include <dht.h>
-
-
 #define dht_apin A0 // Analog Pin sensor is connected to
  
 dht DHT;
- 
+float previous_value = -1;
+int counter = 0;
+int stabilization_counter = 0;
+int stablization_threshold = 5; //todo maybe change this to a higher number
+
 void setup(){
  
   Serial.begin(9600);
@@ -13,17 +15,33 @@ void setup(){
   delay(1000);//Wait before accessing Sensor
  
 }//end "setup()"
- 
+
 void loop(){
   //Start of Program 
  
     DHT.read11(dht_apin);
-    
+    float dht_temperature = DHT.temperature;
+    Serial.println(((dht_temperature - 1) * 1.8) + 32); // subtracting a 1 for more accurate temperature results 
 
-    Serial.print("temperature = ");
-    Serial.print((DHT.temperature * 1.8) + 32); 
-    Serial.println("F  ");
+    if(dht_temperature == previous_value)
+    {
+      counter ++;
+    }
+    else
+    {
+      counter = 0;
+    }
+    previous_value = dht_temperature;
     
+    if (counter >= stablization_threshold)
+    {
+      Serial.print("The temperature has been stabalized!");
+      stabilization_counter ++;
+      if(stabilization_counter > 0)
+      {
+        Serial.print(((dht_temperature - 1) * 1.8) + 32); // subtracting a 1 for more accurate temperature results 
+      }
+    }
     delay(10000);//Wait 10 seconds before accessing sensor again.
  
   //Fastest should be once every two seconds.
